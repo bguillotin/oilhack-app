1ed9c3fd1c2c7a9fef5fa7651f0884ea6d0a0591
benoit GUILLOTIN <benoit@lumapps.com>
2018-11-27 09:15:30 +0100

    feat(videos): integrate vimeo

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-const expresApp = require('express')();
-const server = require('http').Server(expresApp)
+const expressApp = require('express')();
+const server = require('http').Server(expressApp)
 const next = require('next');
 const { parse } = require('url');
-const io = require('socket.io')(server);
+// const io = require('socket.io')(server);
+const Vimeo = require('vimeo').Vimeo;
 
 const dev = process.env.NODE_ENV !== 'production';
 const nextApp = next({ dev });
@@ -12,22 +13,38 @@ const handle = nextApp.getRequestHandler();
 require('dotenv').load()
 const { version } = require('./package.json');
 
-// Send a message to cliente
-const messages = [];
+const { VIMEO_ID, VIMEO_SECRET, VIMEO_TOKEN } = process.env;
+console.log('Here is values for  VCID', VIMEO_ID);
 
-// socket.io server
-io.on('connection', socket => {
-    socket.emit('now', {
-        message: version,
-    });
-});
+const clientV = new Vimeo(VIMEO_ID, VIMEO_SECRET, VIMEO_TOKEN);
 
 nextApp.prepare()
 .then(() => {
-    expresApp.get('*', (req, res) => {
-        const parsedUrl = parse(req.url, true);
-        const { pathname, query } = parsedUrl;
+    expressApp.get('/vimeo', (req, res) => {    
+        clientV.request({
+            path: '/me/appearances',
+            query: {
+                page: 1,
+                per_page: 10,
+                fields: 'uri,name,description,duration,created_time,modified_time'
+            }
+        }, function (error, body, status_code, headers) {
+            if (error) {
+                console.log('error');
+                console.log(error);
+            }
+            
+            console.log('status code');
+            console.log(status_code);
+            
+            res.setHeader('content-type', 'application/json');
+            res.send(JSON.stringify(body));
+        });             
+    });
 
+    expressApp.get('*', (req, res) => {
+        const parsedUrl = parse(req.url, true);
+        const { pathname, query } = parsedUrl;        
         return handle(req, res);
     });
 
